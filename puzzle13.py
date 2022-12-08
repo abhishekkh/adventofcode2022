2336c89a49ff553a7dbdd86dc0c46109047da51d
ahodavdekar <ahodavdekar@marqeta.com>
2022-12-07 18:28:11 -0600

    add bfs

--- puzzle13.py
+++ puzzle13.py
@@ -2,12 +2,13 @@
 # / - root
 # $ - instruction
 #   - cd [.., dirname]
-#   - ls 
+#   - ls
 #       - dir dir_name
 #       - file_size file_name
 # On cd traverse the tree - either up or down to a node
 # On ls add nodes to the tree and update the directory sizes all the way up to the root.
 # Finally traverse down the tree from root and find dir sizes > 10K
+
 
 class Node:
     is_dir = False
@@ -20,10 +21,10 @@ class Node:
         self.name = name
         self.children = []
         self.parent = parent
-        
+
 
 def handle_ls(current_node: Node, ls_output: list):
-    #print("ls output", ls_output)
+    # print("ls output", ls_output)
     if current_node is None:
         raise Exception("Cannot ls into when node is None")
 
@@ -32,22 +33,21 @@ def handle_ls(current_node: Node, ls_output: list):
             new_node = Node(True, output[1], 0, current_node)
         else:
             new_node = Node(False, output[1], int(output[0]), current_node)
-        
+
         current_node.children.append(new_node)
 
 
 def handle_cd(current_node: Node, new_dir: str) -> Node:
-    #print("change dir: ", new_dir)
+    # print("change dir: ", new_dir)
     if new_dir == "..":
         # traverse up a node
-        return current_node.parent    
+        return current_node.parent
     else:
         # traverse down a node
-        for i,child in enumerate(current_node.children):
+        for i, child in enumerate(current_node.children):
             if child.name == new_dir:
                 return current_node.children[i]
 
-         
 
 def build_file_system():
     output = []
@@ -59,7 +59,7 @@ def build_file_system():
             input = line.strip()
             command = input.split()
 
-            if(input.startswith("$")):
+            if input.startswith("$"):
                 if lsoutput:
                     handle_ls(current_node, output)
                     lsoutput = False
@@ -76,78 +76,61 @@ def build_file_system():
             else:
                 lsoutput = True
                 output.append(command)
-            
+
     handle_ls(current_node, output)
     return root_node
+
 
 def dfs(node: Node) -> int:
     if node.is_dir:
         for child in node.children:
-            node.size+=dfs(child)
-        if node.size >= 268565 and node.size <= 300000:
-            print("node: ", node.name, node.size)
+            node.size += dfs(child)
 
     return node.size
 
-def calculate_sum(node: Node, total: int) -> int:
-    if node.is_dir:        
-        for child in node.children:
-            total+=calculate_sum(child, total)
-        
-        if node.size <= 100000:
-            return node.size
-        else:
-            return total
-    else:
-        return total
-        
+
+def compare_size(size: int, wanted: int, condition: str) -> bool:
+    if condition == "<=":
+        return size <= wanted
+    if condition == ">=":
+        return size >= wanted
+
+
+def bfs(node: Node, wanted: int, comp: str) -> tuple:
+    queue = []
+    queue.append(node)
+    visited = set()
+    visited.add(node)
+    total = 0
+    wanted_list = []
+    while len(queue) > 0:
+        curr = queue.pop(0)
+        if curr.is_dir and compare_size(curr.size, wanted, comp):
+            total += curr.size
+            wanted_list.append(curr.size)
+
+        for child in curr.children:
+            if child not in visited:
+                queue.append(child)
+                visited.add(child)
+
+    return total, wanted_list
+
 
 def main():
     root = build_file_system()
     # calculate file sizes
     dfs(root)
-    print(root.size)
-    print("unused space", 70000000 - root.size)
-    space_needed = 30000000 - (70000000 - root.size)
-    print("space needed",space_needed)
-    #total_sum = calculate_sum(root, 0)
-    #print(total_sum)
 
-inp = [92978,
-955,
-22963,
-60566,
-99044,
-80072,
-82485,
-82485,
-32343,
-55058,
-18509,
-8326,
-8326,
-25082,
-56794,
-14981,
-283,
-63263,
-88980,
-88980,
-93470,
-42813,
-1238,
-44239,
-3286,
-40920,
-78417,
-47522,
-98558]
+    total_sum, wanted_list = bfs(root, wanted=100000, comp="<=")
+    print("total: ", total_sum)
+
+    print("unused space:", 70000000 - root.size)
+    space_needed = 30000000 - (70000000 - root.size)
+    print("space needed:", space_needed)
+    total_sum, wanted_list = bfs(root, wanted=space_needed, comp=">=")
+    print("directory to be evicted:", min(wanted_list))
 
 
 if __name__ == "__main__":
     main()
-    total = 0
-    for x in inp:
-        total+=x
-    print(total)
-    
